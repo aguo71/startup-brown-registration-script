d7a8878c6fb191b1184bcffc6c9f1fcace9d1627
Ashley Chang <ashley_e_chang@brown.edu>
2021-03-26 21:30:48 -0400

    current changes

---
+++ README.md
@@ -0,0 +1,4 @@
+# How to build/run the program
+set up virtual environment -> then
+`pip3 install -r requirements.txt`
+to run file do `python main.py`
--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import pandas as pd
 # replace with your filename
 df = pd.read_excel("/Users/ashleychang/PycharmProjects/registration-startup/first_version_of_startup_registration.xlsx")
 df = df[['Name', 'School Email', 'Please rank your *Workshop I* preferences below!']]
-df['workshop list'] = (df['Please rank your *Workshop I* preferences below!'].split(","))
+df['workshop list'] = df.apply(lambda x: x['Please rank your *Workshop I* preferences below!'].split(","))
 print(df)
 
